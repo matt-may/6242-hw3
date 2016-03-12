8fd1e06b8895fcb1f0ded700bf9ef338e094b1d4
Matt May <matthewbentonmay@gmail.com>
2016-03-12 16:05:45 -0600

    working on new branch

--- hw3-skeleton/Task1/run1.sh
+++ hw3-skeleton/Task1/run1.sh
@@ -1,3 +1,3 @@
-hadoop jar ./target/task1-1.0.jar edu.gatech.cse6242.Task1 /user/cse6242/graph1.tsv /user/cse6242/task1output1
-hadoop fs -getmerge /user/cse6242/task1output1/ task1output1.tsv
-hadoop fs -rm -r /user/cse6242/task1output1
+hadoop jar ./target/task1-1.0.jar edu.gatech.cse6242.Task1 /user/me/graph1b.tsv /user/me/task1output1
+hadoop fs -getmerge /user/me/task1output1/ task1boutput1-2.tsv
+hadoop fs -rm -r /user/me/task1output1/
--- hw3-skeleton/Task1/src/main/java/edu/gatech/cse6242/Task1.java
+++ hw3-skeleton/Task1/src/main/java/edu/gatech/cse6242/Task1.java
@@ -1,5 +1,6 @@
 package edu.gatech.cse6242;
 
+import java.util.*;
 import java.io.IOException;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.Configuration;
@@ -11,7 +12,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Task1 {
   public static class GraphMapper
-   extends Mapper<Object, Text, IntWritable, IntWritable> {
+   extends Mapper<Object, Text, IntWritable, Text> {
 
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
@@ -20,41 +21,81 @@ public class Task1 {
       String line = value.toString();
       String[] tkns = line.split("\t");
 
-      // Save our src node ID, and our weight.
-      IntWritable src = new IntWritable(Integer.parseInt(tkns[0]));
-      IntWritable weight = new IntWritable(Integer.parseInt(tkns[2]));
+      int src = Integer.parseInt(tkns[0]);
+      int tgt = Integer.parseInt(tkns[1]);
 
-      // Write.
-      context.write(src, weight);
+      IntWritable wsrc = new IntWritable(src);
+      Text wtgt = new Text(Integer.toString(tgt) + "|1");
+      context.write(wsrc, wtgt);
+
+      IntWritable wsrc2 = new IntWritable(tgt);
+      Text wtgt2 = new Text(Integer.toString(src) + "|0");
+      context.write(wsrc2, wtgt2);
     }
   }
 
   public static class EdgeWeightReducer
-     extends Reducer<IntWritable,IntWritable,IntWritable,IntWritable> {
-    private IntWritable result = new IntWritable();
+     extends Reducer<IntWritable,Text,IntWritable,Text> {
+    // private IntWritable result = new IntWritable();
 
-    public void reduce(IntWritable key, Iterable<IntWritable> values,
+    public void reduce(IntWritable key, Iterable<Text> values,
                        Context context
                        ) throws IOException, InterruptedException {
 
-      // Initialize our max to the first value.
-      int max = values.iterator().next().get();
-
       // Go through all our values - if the current value is greater
       // than the max, set our max to the current value.
-      for (IntWritable val : values) {
-        int getVal = val.get();
+      List<String> inNodes = new ArrayList<String>();
+      List<String> outNodes = new ArrayList<String>();
 
-        if (getVal > max) {
-          max = getVal;
-        }
+      for (Text val : values) {
+        String v = val.toString();
+        //String[] tkns = v.split("|");
+
+        Text myText = new Text(v);
+
+        context.write(key, myText);
+        // if (Integer.parseInt(tkns[1]) == 0) {
+        //   inNodes.add(tkns[1]);
+        // }
+        // else {
+        //   outNodes.add(tkns[1]);
+        // }
       }
 
-      // Set the result.
-      result.set(max);
+      // for (String inNode : inNodes) {
+      //   Text outVal = new Text(inNode);
+      //
+      //   context.write(key, outVal);
+      // }
 
-      // Write the result.
-      context.write(key, result);
+      // for (String outNode : outNodes) {
+      //   Text outVal2 = new Text(outNode);
+      //
+      //   context.write(key, outVal2);
+      // }
+
+      //
+      // IntWritable a = new IntWritable(1);
+      // String str = "my string";
+      // Text b = new Text(str);
+      //
+      // context.write(a, b);
+      //
+      // for (String inNode : inNodes) {
+      //   IntWritable keyOut = new IntWritable(Integer.parseInt(inNode));
+      //
+      //   for (String outNode : outNodes) {
+      //     System.out.println("outer loopssssss");
+      //
+      //     if (inNode != outNode) {
+      //       System.out.println("inner looops ");
+      //
+      //       Text valOut = new Text(outNode);
+      //
+      //       context.write(keyOut, valOut);
+      //     }
+      //   }
+      // }
     }
   }
 
@@ -67,8 +108,10 @@ public class Task1 {
     job.setMapperClass(GraphMapper.class);
     job.setCombinerClass(EdgeWeightReducer.class);
     job.setReducerClass(EdgeWeightReducer.class);
-    job.setOutputKeyClass(IntWritable.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setMapOutputKeyClass(IntWritable.class);
+    job.setMapOutputValueClass(Text.class);
+    // job.setOutputKeyClass(IntWritable.class);
+    // job.setOutputValueClass(Text.class);
 
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
